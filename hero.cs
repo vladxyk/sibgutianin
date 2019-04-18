8518c50442eafdbd6a194bb8de4c3233b10a6f79
kaelhell <goriunova00@gmail.com>
2019-04-18 21:31:28 +0700

    add basic move hero

---
+++ hero.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent <Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rigidbody.MovePosition(rigidbody.position + Vector2.right * moveX * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigidbody.AddForce(Vector2.up * 15000);
+    }
+}
