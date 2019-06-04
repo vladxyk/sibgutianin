9721b9f1f97d8dacdee7fdd550248fe1b27770e1
Kaelhell <goriunova00.com>
2019-06-04 15:08:43 +0700

    add scripts(add bonus)

---
+++ assets/scripts/camera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject hero;
+
+    void Update()
+    {
+        transform.position = new Vector3(hero.transform.position.x, hero.transform.position.y, -10f);
+    }
+}
---
+++ assets/scripts/hero.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D rigidbody;
+    bool goRight = true;
+    public int score;
+   //* public Rect(
+       
+    void Start()
+    {
+        rigidbody = GetComponent <Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rigidbody.MovePosition(rigidbody.position + Vector2.right * moveX * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigidbody.AddForce(Vector2.up * 20000);
+
+        if (moveX > 0 && !goRight)
+            flip();
+        else if (moveX < 0 && goRight)
+            flip();
+    }
+
+    void flip()
+    {
+        goRight = !goRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D bonus) { 
+
+        if (bonus.gameObject.tag == "bonus")
+        {
+            Destroy(bonus.gameObject);
+            score++;
+        }
+    }
+
+    
+}
