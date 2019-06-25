d5264d4159031e1d7f3d35e36575d2a753c7ed09
Kaelhell <goriunova00.com>
2019-06-25 07:32:43 +0700

    Add interactive menu and game over

---
+++ Assets/menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void isStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void isQuit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
